e3c56665e253edfa9aa4a12b78febc2c794d15cb
atsolberg <atsolberg@gmail.com>
2021-03-25 11:30:38 -0500

    moved item ids scrape to function

--- src/scraper/index.js
+++ src/scraper/index.js
@@ -8,6 +8,30 @@ function filterRange(from, to) {
   return `${items_url}?filter=151:151;2:4;${from}:${to}`;
 }
 
+async function scrapeItemIds(page) {
+  let items = [];
+
+  let from = 1;
+  let to = 1000;
+  for (let i = 1; i <= 25; i++) {
+    const url = filterRange(from, to);
+    console.log(`navigating to: `, url, '\n');
+    await page.goto(url);
+    const rowData = await parsePageForItems(page);
+    items = [...items, ...rowData];
+    from = from + 1000;
+    to = to + 1000;
+  }
+
+  if (items.length) {
+    console.log(`found ${items.length} items`);
+    const json = JSON.stringify(items, null, 2);
+    fs.writeFileSync('../db/items.json', json);
+  } else {
+    console.error('No items scraped!');
+  }
+}
+
 /**
  * Without aborting some requests that are 'pending' forever,
  * some page actions will hang forever.
@@ -46,27 +70,7 @@ puppeteer
     await page.setRequestInterception(true);
     page.on('request', ignoreGarbage);
 
-    let items = [];
-
-    let from = 1;
-    let to = 1000;
-    for (let i = 1; i <= 25; i++) {
-      const url = filterRange(from, to);
-      console.log(`navigating to: `, url, '\n');
-      await page.goto(url);
-      const rowData = await parsePageForItems(page);
-      items = [...items, ...rowData];
-      from = from + 1000;
-      to = to + 1000;
-    }
-
-    if (items.length) {
-      console.log(`found ${items.length} items`);
-      const json = JSON.stringify(items, null, 2);
-      fs.writeFileSync('../db/items.json', json);
-    } else {
-      console.error('No items scraped!');
-    }
+    scrapeItemIds(page);
   })
   .catch(function(err) {
     //handle error
